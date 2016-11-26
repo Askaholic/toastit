65d0880b8e19b72c199fa8e24a0d17547dbcec64
Rohan E Weeden <reweeden@ua.alaska.edu>
2016-11-25 16:18:04 -0900

    The first iteration of the website. The biggest issue is the login link not displaying in the correct place

---
+++ assets/js/custom.js
@@ -0,0 +1,92 @@
+
+
+/*=============================================================
+    Authour URI: www.binarytheme.com
+    License: Commons Attribution 3.0
+
+    http://creativecommons.org/licenses/by/3.0/
+
+    100% To use For Personal And Commercial Use.
+    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+   
+    ========================================================  */
+
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+            /*====================================
+               CUSTOM LINKS SCROLLING FUNCTION 
+              ======================================*/
+
+            $('a[href*=#]').click(function () {
+                if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+               && location.hostname == this.hostname) {
+                    var $target = $(this.hash);
+                    $target = $target.length && $target
+                    || $('[name=' + this.hash.slice(1) + ']');
+                    if ($target.length) {
+                        var targetOffset = $target.offset().top;
+                        $('html,body')
+                        .animate({ scrollTop: targetOffset }, 800); //set scroll speed here
+                        return false;
+                    }
+                }
+            });
+            //SLIDE SHOW SCRIPT
+
+            $('.carousel').carousel({
+                interval: 3000 //TIME IN MILLI SECONDS
+            })
+
+          
+            /*====================================
+               WRITE YOUR SCRIPTS BELOW 
+           ======================================*/
+            
+            $('input[type="submit"]').mousedown(function(){
+              $(this).css('background', '#2ecc71');
+            });
+            $('input[type="submit"]').mouseup(function(){
+              $(this).css('background', '#1abc9c');
+            });
+
+            $('#loginform').click(function(){
+              $('.login').fadeToggle('slow');
+              $(this).toggleClass('green');
+            });
+
+
+
+            $(document).mouseup(function (e)
+            {
+                var container = $(".login");
+
+                if (!container.is(e.target) // if the target of the click isn't the container...
+                    && container.has(e.target).length === 0) // ... nor a descendant of the container
+                {
+                    container.hide();
+                    $('#loginform').removeClass('green');
+                }
+            });
+
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
+
+
+
